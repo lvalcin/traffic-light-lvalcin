586c652d57b7ac9ed54289b855f6b922fb58d81c
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-11 02:58:51 +0000

    traffic light created

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,18 +2,20 @@ import React , {useState} from "react";
 
 
 const Home = () => {
-	const [ color, setColor ] = useState("red");
+	const [ red, setRed ] = useState("red");
+	const [yellow, setYellow] = useState("yellow");
+	const [green, setGreen] = useState("green");
 	
 	
 /* <div class="shadow-lg p-3 mb-5 bg-white rounded">Larger shadow</div> */
 	
 	return (
 	
-		<div className="text-center">
-			<div className= "rectangle trafficLight">
-					<div className ="circle redLight border border-danger rounded-circle"></div>
-					<div className="circle yellowLight border border-warning rounded-circle"></div>
-					<div className="circle greenLight border border-success rounded-circle"></div>
+		<div className="text-center row">
+			<div className= "rectangle trafficLight border bg-dark col-1 mx-auto">
+					<div className ="circle redLight border border-danger bg-danger rounded-circle"></div>
+					<div className="circle yellowLight border border-warning bg-warning rounded-circle"></div>
+					<div className="circle greenLight border border-success bg-success rounded-circle"></div>
 			</div>	
         
 
