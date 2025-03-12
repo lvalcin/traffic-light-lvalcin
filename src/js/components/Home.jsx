7bb1eac4b1db37b5c05401371403f0a7c69bc6be
Lovnyth Valcin <lovnyth@gmail.com>
2025-03-12 00:22:04 +0000

    conditional ternary statement added for each light color along with onClick function

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,20 +2,14 @@ import React , {useState} from "react";
 
 
 const Home = () => {
-	const [ red, setRed ] = useState("red");
-	const [yellow, setYellow] = useState("yellow");
-	const [green, setGreen] = useState("green");
-	
-	
-/* <div class="shadow-lg p-3 mb-5 bg-white rounded">Larger shadow</div> */
-	
+	const [ color, setColor ] = useState("red")
+
 	return (
-	
-		<div className="text-center row">
-			<div className= "rectangle trafficLight border bg-dark col-1 mx-auto">
-					<div className ="circle redLight border border-danger bg-danger rounded-circle"></div>
-					<div className="circle yellowLight border border-warning bg-warning rounded-circle"></div>
-					<div className="circle greenLight border border-success bg-success rounded-circle"></div>
+		<div className="text-center">
+			<div className= "trafficlight border bg-dark d-inline-block p-2 ">
+					<div className ={'circle redLight bg-danger rounded-circle '+(color==="red"?"redShadow":"")} onClick={()=>setColor("red")}></div>
+					<div className={'circle yellowLight bg-warning rounded-circle ' + (color==="yellow"?"yellowShadow":"")} onClick={()=>setColor("yellow")}></div>
+					<div className={'circle greenLight bg-success rounded-circle ' + (color==="green"?"greenShadow":"")} onClick={()=>setColor("green")}></div>
 			</div>	
         
 
@@ -24,3 +18,4 @@ const Home = () => {
 }
 
 export default Home;
+// ReactDOM.render(<TrafficLight />, document.querySelector('#app'));
